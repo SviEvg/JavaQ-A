07c8784fd2180e1e3d6c4330a50d1f0110d0cb0e
SviEvg <svishov80@bk.ru>
2023-02-07 22:55:57 +0300

    основное меню

--- src/main/java/com/project/javaQA/service/TelegramBot.java
+++ src/main/java/com/project/javaQA/service/TelegramBot.java
@@ -4,17 +4,37 @@ import com.project.javaQA.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
   final   BotConfig config;
+  static final String HELP_TEXT = "JavaQ&A - бот, который вобрал в себя основные вопросы по Java для интервью.\n" +
+          "В каждом разделе собраны наиболее популярные вопросы по выбранной теме.";
 
     public TelegramBot(BotConfig config) {
+
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "приветствие"));
+        listOfCommands.add(new BotCommand("/section", "выбор раздела"));
+        listOfCommands.add(new BotCommand("/help", "информация о JavaQ&A"));
+        try{
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
     @Override
@@ -37,11 +57,14 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/start":
 
                         startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                        break;
 
-                 break;
+               case "/help":
+                       sendMessage(chatId, HELP_TEXT);
+                       break;
                 default:
 
-                        sendMessage(chatId, "Sorry, command was not recognized");
+                        sendMessage(chatId, "Извините! Данная команда не поддерживается!");
 
 
             }
@@ -50,7 +73,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
     private void startCommandReceived(long chatId, String name) {
 
-        String answer = "HI,  " + name + ", nice to meet you! ";
+        String answer = "Здраствуйте,  " + name + "! Выберите раздел! ";
         log.info("Replied to user " + name);
         sendMessage(chatId, answer);
     }
